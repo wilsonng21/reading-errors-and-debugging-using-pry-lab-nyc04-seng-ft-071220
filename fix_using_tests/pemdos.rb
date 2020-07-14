311047a2800ab440fff6f1612dfaba91ff95a700
Wilson Ng <wilsongch92@gmail.com>
2020-07-14 19:26:10 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,15 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
+  binding.pry
   if string[0] == "s"
-    10 * "s" + string
+    s = ssssssssss
+    binding.pry
+    s + string
+    binding.pry
   else
+    binding.pry
     string
   end
 end
