5baf6d989af1d2888a3efe0e3c462d8fe14fbca6
Wilson Ng <wilsongch92@gmail.com>
2020-07-14 19:41:03 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,10 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    beginning = "ssssssssss"
+    beginning + string
   else
     string
   end
